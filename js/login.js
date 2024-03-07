5ce64a39dd4e73e0adecdcff0ddb5b28eb7ea3b9
unknown <pilartaraizamara@gmail.com>
2024-03-07 11:01:59 +0800

    first com

---
+++ js/login.js
@@ -0,0 +1,21 @@
+function login () {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if(username === "" || password === "") {
+        document.getElementById("error").innerHTML = "All textboxes is required";
+    }
+    else{
+        if(username === real_username && password === real_password) {
+            document.getElementById("error").innerHTML = "Welcome " + username;
+        }
+
+        else{
+            document.getElementById("error").innerHTML = "Invalid username or password";
+        }
+    }
+}
+
+function forgot() {
+    alert("You clicked forgot password");
+}
